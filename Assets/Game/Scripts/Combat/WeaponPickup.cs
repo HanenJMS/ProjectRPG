758779644ab0b58dc19779c2491365fb2abdbd00
HanenJMS <Hanensgamelab@gmail.com>
2022-07-08 12:22:32 -0400

    7/8 RPG Combat Core Section 10, part 127 Pickup Respawning. Allowing for items to be picked up, disabled for a set duration, and respawned after.

--- Assets/Game/Scripts/Combat/WeaponPickup.cs
+++ Assets/Game/Scripts/Combat/WeaponPickup.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,13 +8,28 @@ namespace RPG.Combat
     public class WeaponPickup : MonoBehaviour
     {
         [SerializeField] Weapon weapon = null;
+        [SerializeField] float respawnTime = 5f;
         private void OnTriggerEnter(Collider other)
         {
             Fighter fighter = other.GetComponent<Fighter>();
             if(fighter != null)
             {
                 fighter.EquipWeapon(weapon);
-                Destroy(gameObject);
+                StartCoroutine(HideForSeconds(respawnTime));
+            }
+        }
+        IEnumerator HideForSeconds(float seconds)
+        {
+            ShowPickup(false);
+            yield return new WaitForSeconds(seconds);
+            ShowPickup(true);
+        }
+        private void ShowPickup(bool shouldShow)
+        {
+            GetComponent<SphereCollider>().enabled = shouldShow;
+            foreach(Transform child in transform)
+            {
+                child.gameObject.SetActive(shouldShow);
             }
         }
     }
