ef3156a47c5bed074406c500fcbbc62df5c0c060
HanenJMS <Hanensgamelab@gmail.com>
2022-07-19 22:12:24 -0400

    7/19 Character UI's can now update Healthbar scaling.

---
+++ Assets/Game/Scripts/Attributes/HealthBar.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace RPG.Attributes
+{
+    public class HealthBar : MonoBehaviour
+    {
+        [SerializeField] RectTransform healthBar = null;
+        [SerializeField] Health health = null;
+        [SerializeField] Image defaultImage = null;
+        float maxScale = 1f;
+        Color defaultColor;
+        private void Awake()
+        {
+
+        }
+        private void Start()
+        {
+            defaultColor = defaultImage.color;
+        }
+        private void Update()
+        {
+            healthBar.localScale = GetHealthSize(health.GetPercentage());
+            if (health.GetPercentage() <= 20)
+            {
+                defaultImage.color = Color.red;
+            }
+            else if (health.GetPercentage() <= 50)
+            {
+                defaultImage.color = (Color.yellow);
+            }
+            else
+            {
+                defaultImage.color = defaultColor;
+            }
+            if (health.GetPercentage() <= 0)
+            {
+                gameObject.SetActive(false);
+            }
+        }
+        private Vector3 GetHealthSize(float health)
+        {
+            return new Vector3((health / 100 * maxScale), maxScale, maxScale);
+        }
+    }
+}
